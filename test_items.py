339b41d71104ced08c517d963f418bc8095fabac
locq98 <97903094+locq98@users.noreply.github.com>
2022-02-17 16:32:28 +0300

    Fixed errors in the task v.2

--- test_items.py
+++ test_items.py
@@ -1,8 +1,8 @@
 import time
 from selenium.webdriver.common.by import By
 
-class TestLanguageButton():
-    def test_buy_button_es(self, browser):
+class TestButton():
+    def test_purchase_button(self, browser):
         link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
         browser.get(link)
         button = len(browser.find_elements(By.CSS_SELECTOR, "button.btn:nth-child(3)"))
